245c3f35aaffbb5d0115e6af74cca822953bfa6a
xu_xz <xxz0528@qq.com>
2025-07-14 08:41:25 +0800

    opt: 修改终端启动快捷键

--- modules/hotkeys.lua
+++ modules/hotkeys.lua
@@ -11,8 +11,8 @@ function module.init()
 	hs.alert.show("hotkeys init")
   -- 定义热键绑定
   -- 例如：将 Command + Shift + T 绑定到打开 Terminal 应用程序的热键
-  -- 注意：在 macOS 中，Command 键通常被称为 Cmd 键，而 Shift 键通常被称为 Shift 键
-  hs.hotkey.bind({"cmd", "shift"}, "T", _launchTerminal)
+  -- 注意：在 macOS 中，Option键通常呗称为alt,Command 键通常被称为 Cmd 键，而 Shift 键通常被称为 Shift 键
+  hs.hotkey.bind({"alt", "shift"}, "T", _launchTerminal)
   -- 其他热键绑定...
 end
 
